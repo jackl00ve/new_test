bfc74b8a5459bcc354db657745813be533870326
jackl00ve <19392290424@163.com>
2026-03-16 10:37:10 +0800

    modified str_num

--- add.cpp
+++ add.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
 
+int str_num(int num,std::string brr)
+    {
+        for(int i = 0 ; i < num ; ++i)
+        {
+            std::cout<<brr<<std::endl;
+        }
+    }
 int main() {
     std::cout << "你好世界" << std::endl;
+    
+    str_num(5,"Hello World");
+
     
     return 0;
 }
